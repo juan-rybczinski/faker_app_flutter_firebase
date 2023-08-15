541d245455e86871a493e4bdf67dec78b97b44e3
juan-rybczinski <jhreplay.lee@gmail.com>
2023-08-15 13:17:53 +0900

    Writing a Cloud Function Trigger

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,21 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+import * as admin from "firebase-admin"
+import * as logger from "firebase-functions/logger"
+import * as functions from "firebase-functions/v2"
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+admin.initializeApp()
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+export const makeJobTitleUppercase = functions.firestore.onDocumentWritten("/users/{uid}/jobs/{jobId}", (event) => {
+    const change = event.data
+    if (change === undefined) {
+        return
+    }
 
-export const helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+    const data = change.after.data()
+    if (data === undefined) {
+        return
+    }
+
+    const uppercase = data.title.toUpperCase()
+    logger.log(`Uppercasing ${change.after.ref.path}: ${data.title} => ${uppercase}`)
+    return change.after.ref.set({title: uppercase}, {merge: true})
+})
