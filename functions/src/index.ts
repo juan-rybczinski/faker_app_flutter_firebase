a094fb09ffd6b0df3a669344ffe271480045a4da
juan-rybczinski <jhreplay.lee@gmail.com>
2023-08-15 16:49:41 +0900

    Deploying Cloud Functions with the Firebase CLI
    firebase deploy --only functions
    firebase functions:delete functionName

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,36 +5,36 @@ import * as functions from "firebase-functions/v2"
 admin.initializeApp()
 
 export const makeJobTitleUppercase = functions.firestore.onDocumentWritten("/users/{uid}/jobs/{jobId}", (event) => {
-    const change = event.data
-    if (change === undefined) {
-        return
-    }
+  const change = event.data
+  if (change === undefined) {
+    return
+  }
 
-    const data = change.after.data()
-    if (data === undefined) {
-        return
-    }
+  const data = change.after.data()
+  if (data === undefined) {
+    return
+  }
 
-    const uppercase = data.title.toUpperCase()
-    if (uppercase === data.title) {
-        return
-    }
+  const uppercase = data.title.toUpperCase()
+  if (uppercase === data.title) {
+    return
+  }
 
-    logger.log(`Uppercasing ${change.after.ref.path}: ${data.title} => ${uppercase}`)
-    return change.after.ref.set({title: uppercase}, {merge: true})
+  logger.log(`Uppercasing ${change.after.ref.path}: ${data.title} => ${uppercase}`)
+  return change.after.ref.set({ title: uppercase }, { merge: true })
 })
 
 export const deleteAllUserJobs = functions.https.onCall(async (context: functions.https.CallableRequest) => {
-    const uid = context.auth?.uid
-    if (uid === undefined) {
-        throw new functions.https.HttpsError("unauthenticated", "You need to be authenticated to perform this action")
-    }
-    const firestore = admin.firestore()
-    const collectionRef = firestore.collection(`/users/${uid}/jobs`)
-    const docRefs = await collectionRef.listDocuments()
-    for (const docRef of docRefs) {
-        await docRef.delete()
-    }
-    logger.log(`Deleted ${docRefs.length} docs at ${collectionRef.path}`)
-    return {"count": docRefs.length}
+  const uid = context.auth?.uid
+  if (uid === undefined) {
+    throw new functions.https.HttpsError("unauthenticated", "You need to be authenticated to perform this action")
+  }
+  const firestore = admin.firestore()
+  const collectionRef = firestore.collection(`/users/${uid}/jobs`)
+  const docRefs = await collectionRef.listDocuments()
+  for (const docRef of docRefs) {
+    await docRef.delete()
+  }
+  logger.log(`Deleted ${docRefs.length} docs at ${collectionRef.path}`)
+  return { "count": docRefs.length }
 })
